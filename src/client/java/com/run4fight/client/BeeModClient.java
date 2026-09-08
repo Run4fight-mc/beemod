96c91b2bfeb55d0f9efb584879e6ea4f7bc048b5
Run4fight <Run4fight2005@gmail.com>
2026-09-08 10:00:58 +0200

    - Creation of project base

---
+++ src/client/java/com/run4fight/client/BeeModClient.java
@@ -0,0 +1,10 @@
+package com.run4fight.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeeModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
